9103ef46888548e6ff6aed09a1027ab1a80b7733
Khiem Nguyen <ng.khiem97vn@gmail.com>
2023-02-05 02:06:09 -0500

    debug

--- read_saved_np.py
+++ read_saved_np.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-front_ex = np.load("saves/cameras/side_2/matrices/ex_2023_2_4_17_28_54.npy")
-front_in = np.load("saves/cameras/side_2/matrices/in_2023_2_4_17_28_54.npy")
+front_ex = np.load("saves/cameras/side_2/matrices/ex_2023_2_5_0_0_9.npy")
+front_in = np.load("saves/cameras/side_2/matrices/in_2023_2_5_0_0_9.npy")
 
 print(front_ex)
 print(front_in)
